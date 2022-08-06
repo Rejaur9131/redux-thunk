d4d1fb24124516dc24b0a592159a4d996009d32f
MD REJAUR RAHMAN <you@example.com>
2022-08-06 20:15:48 +0600

    redux-thunk prctice

---
+++ index.js
@@ -0,0 +1,81 @@
+// https://jsonplaceholder.typicode.com/todos
+
+const { createStore, applyMiddleware } = require("redux");
+const { default: thunk } = require("redux-thunk");
+
+// constants
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_FAILED = "GET_TODOS_FAILED";
+
+// States
+const initialTodoState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+// Actions
+const getTodosRequest = () => {
+  return {
+    type: GET_TODOS_REQUEST,
+  };
+};
+
+const getTodosSuccess = (todos) => {
+  return {
+    type: GET_TODOS_SUCCESS,
+    payload: todos,
+  };
+};
+
+const getTodosFailed = (error) => {
+  return {
+    type: GET_TODOS_FAILED,
+    payload: error,
+  };
+};
+
+// create reducer
+
+const todosReducer = (state = initialTodoState, action) => {
+  switch (action.type) {
+    case GET_TODOS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case GET_TODOS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        todos: action.payload,
+      };
+
+    case GET_TODOS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//  async action creator
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodosRequest());
+  };
+};
+// store
+const store = createStore(todosReducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(fetchData());
